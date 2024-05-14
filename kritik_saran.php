69edfd32825c7d3577806946583f72233d677c95
Aku-Mars <marifinsyam73@gmail.com>
2024-05-14 16:03:20 +0700

    [~] Memperbaiki PHP

--- kritik_saran.php
+++ kritik_saran.php
@@ -1,29 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $kritik = $_POST['kritik'];
-    $saran = $_POST['saran'];
-
-    $to = "marifinsyam73@gmail.com";
-    $subject = "Kritik & Saran dari $nama";
-    $message = "Nama: $nama\n\n";
-    $message .= "Email: $email\n\n";
-    $message .= "Kritik:\n$kritik\n\n";
-    $message .= "Saran:\n$saran";
-
-    $headers = "From: $email" . "\r\n" .
-               "Reply-To: $email" . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
-
-    if (mail($to, $subject, $message, $headers)) {
-        echo "<script>alert('Pesan berhasil dikirim. Terima kasih atas kritik dan sarannya!');</script>";
-    } else {
-        echo "<script>alert('Pesan gagal dikirim. Mohon coba lagi.');</script>";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -43,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </header>
 
     <div class="container">
-        <form id="kritikForm">
+        <form id="kritikForm" method="post" action="kritik_saran.php">
             <label for="nama">Nama:</label>
             <input type="text" id="nama" name="nama" required>
 
